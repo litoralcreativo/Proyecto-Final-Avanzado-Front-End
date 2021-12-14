1511fac9c7aa384b35f342e5affc7453446ef0d8
Arielstereo <arielstereo@msn.com>
2021-12-14 10:59:14 -0300

    Agregado features y services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const accountModule = () => import('./features/account/account.module').then((m) => m.AccountModule);
+const homeModule = () => import('./features/home/home.module').then((m) => m.HomeModule);
+
+const routes: Routes = [
+  {
+    path: '', 
+    loadChildren: homeModule
+  },
+  {
+    path: 'account', 
+    loadChildren: accountModule
+  },
+  
+  {
+    path: '**',
+    redirectTo: '/account/login'
+  }
+
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { LayoutModule } from './core/layout/layout.module';
 
 
+
 @NgModule({
   declarations: [
-    AppComponent,
-   
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     LayoutModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
